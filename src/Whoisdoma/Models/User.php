e2f859f74f439544dd52e248c3fb0ed4b5cbe02e
Eric <erich@xaos-ia.com>
2014-05-11 14:10:26 -0500

    Fix user model
    Signed-off-by: Eric <erich@xaos-ia.com>

--- src/Whoisdoma/Models/User.php
+++ src/Whoisdoma/Models/User.php
@@ -3,6 +3,8 @@
 namespace Whoisdoma\Models;
 
 use Illuminate\Auth\UserInterface;
+use Auth;
+use Hash;
 
 class User extends Base implements UserInterface 
 {
@@ -21,12 +23,53 @@ class User extends Base implements UserInterface
     
     public function getAuthIdentifier()
     {
-	return $this->getKey();
+	    return $this->getKey();
     }
 
     public function getAuthPassword()
     {
-	return $this->password;
+	    return $this->password;
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+
+    /**
+     * Get the e-mail address where password reminders are sent.
+     *
+     * @return string
+     */
+    public function getReminderEmail()
+    {
+        return $this->email;
     }
 }
 
